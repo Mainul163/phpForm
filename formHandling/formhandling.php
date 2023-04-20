6fc9886116fc2fcb6c7a6d184749b7726fd8fa3b
Mainul islam <mainulisraf163@gmail.com>
2023-04-20 13:19:36 +0600

    first commit

---
+++ formHandling/formhandling.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Page Title</title>
+</head>
+
+<body>
+
+    <h6>Name: <?php  echo $_POST["name"] ;  ?></h6><br />
+    <h6>Email: <?php  echo $_POST["email"] ;  ?></h6>
+</body>
+
+</html>
---
+++ formHandling/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Page Title</title>
+</head>
+
+<body>
+
+    <form action="formhandling.php" method="post">
+        Name: <input type="text" name="name" placeholder="enter your name" required><br>
+        E-mail: <input type="text" name="email" placeholder="enter your email" required><br>
+        <button type="submit">Submit</button>
+    </form>
+
+</body>
+
+</html>
